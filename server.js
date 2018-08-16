acaa1613f55d3fc60aa114e1bb6fab5370e52458
courd <courdheadman@gmail.com>
2018-08-16 19:50:19 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require('express')
+const hbs = require('hbs')
+const fs = require(`fs`)
+
+const app = express()
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs')
+
+app.use((req, res, next) => {
+  const now = new Date().toString()
+  const log = `${now}: ${req.method} ${req.url}`
+  console.log(log)
+  fs.appendFile("server.log", log + '\n', (err) => {
+    if (err) {
+      console.log(`it's brokedededededed`);
+    }
+  })
+  next()
+})
+
+// app.use((req, res, next) => {
+//   res.render('maint.hbs')
+// })
+
+app.use(express.static(__dirname + '/public'))
+
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear()
+})
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase()
+})
+
+app.get('/', (req, res) => {
+  res.render('welcome.hbs', {
+    pageTitle: 'Welcome Page',
+    welcomeMessage: `Welcome to my node site`
+  })
+})
+//
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+  })
+})
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: `Unable to handle request.`
+  })
+})
+
+const portnum = 8080
+
+app.listen(portnum, () => {
+  console.log(`server running on ${portnum}`);
+})
